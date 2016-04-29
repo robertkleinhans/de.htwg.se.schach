f3adccf24c1e09f1c8d3bc359d1c7e38e496597f
robertkleinhans <rokleinh@htwg-konstanz.de>
2016-04-29 12:17:31 +0200

    Added Position-class and trying some unit-tests

--- de.htwg.se.schach/src/de/htwg/se/schach/Position.java
+++ de.htwg.se.schach/src/de/htwg/se/schach/Position.java
@@ -1,5 +1,31 @@
 package de.htwg.se.schach;
 
 public class Position {
-
+	private int x;
+	private int y;
+	
+	
+	public Position(int x, int y) {
+		assert(checkPos(x,y));
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+	
+	public void setPos(int new_x, int new_y) {
+		assert(checkPos(new_x,new_y));
+		this.x = new_x;
+		this.y = new_y;
+	}
+	
+	private boolean checkPos(int x, int y) {
+		return (x < 8 && x >= 0) && (y < 8 && y >= 0);
+	}
 }
