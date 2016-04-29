422832e820f50ea88b952e7e639c9593c933cf44
robertkleinhans <rokleinh@htwg-konstanz.de>
2016-04-29 12:36:30 +0200

    Added testing for Position.java

--- de.htwg.se.schach/test/de/htwg/se/schach/PositionTest.java
+++ de.htwg.se.schach/test/de/htwg/se/schach/PositionTest.java
@@ -5,15 +5,49 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+
+
 public class PositionTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
-
+	Position pos;
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConstructor() {
+		try {
+			pos = new Position(10,8);
+			fail("Position build with invalid values!");
+		} catch (AssertionError e) {
+			assertNotNull(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testGet() {
+		pos = new Position(3,7);
+		assertEquals(3,pos.getX());
+		assertEquals(7,pos.getY());
+	}
+	
+	@Test
+	public void testSetPos() {
+		pos = new Position(1,1);
+		try {
+			pos.setPos(-1,2);
+			fail("Position set to invalid values (-1,2)!");
+			pos.setPos(4, -1);
+			fail("Position set to invalid values (4,-1)!");
+			pos.setPos(-2, 9);
+			fail("Position set to invalid values (-2,9)!");
+			pos.setPos(-5, 11);
+			fail("Position set to invalid values (-5,11)!");
+		} catch (AssertionError e) {
+			assertNotNull(e.getMessage());
+		}
+		
+		pos.setPos(0, 7);
+		assertEquals(0,pos.getX());
+		assertEquals(7,pos.getY());
+		
 	}
 
 }
