d9649489139a24a1917d110040fc1dc06656263a
robertkleinhans <rokleinh@htwg-konstanz.de>
2016-05-13 12:56:55 +0200

    Knight not working completely right now.

--- de.htwg.se.schach/test/de/htwg/se/schach/model/PositionTest.java
+++ de.htwg.se.schach/test/de/htwg/se/schach/model/PositionTest.java
@@ -1,8 +1,7 @@
-package de.htwg.se.schach;
+package de.htwg.se.schach.model;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import de.htwg.se.schach.model.Position;
