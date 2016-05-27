1aae9955e45f64afe68c5ba798da9882a399f601
robertkleinhans <rokleinh@htwg-konstanz.de>
2016-05-27 10:18:20 +0200

    Restructure

--- de.htwg.se.schach/src/de/htwg/se/schach/Chess.java
+++ de.htwg.se.schach/src/de/htwg/se/schach/Chess.java
@@ -1,11 +1,8 @@
 package de.htwg.se.schach;
 
-import de.htwg.se.schach.model.Chessboard;
+
 
 public class Chess {
 	public static void main(String[] args) {
-		Chessboard chess = new Chessboard();
-		System.out.printf("\n-----------\n");
-		chess.printChessboard();
 	}
 }
