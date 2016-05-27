11db8776656171c84df47e09d07822869e3edc32
robertkleinhans <rokleinh@htwg-konstanz.de>
2016-05-27 10:29:46 +0200

    Adding Rook.

---
+++ de.htwg.se.schach/src/de/htwg/se/schach/model/Rook.java
@@ -0,0 +1,29 @@
+package de.htwg.se.schach.model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Rook extends Piece {
+    public Rook(int row, int column, int team) {
+        super(team);
+        this.row = row;
+        this.column = column;
+        this.cut = "RO";
+    }
+    
+    public List<Position> getAllMoves() {
+        List<Position> ret = new LinkedList<Position>();
+        
+        for (int i = 0; i <= MAX_ROW; i++) {
+            ret.add(new Position(i,column));
+            ret.add(new Position(row,i));
+        }
+        Position own = new Position(row,column);
+        while(ret.contains(own)) {
+            ret.remove(own);
+        }
+        
+        return ret;
+    }    
+}
