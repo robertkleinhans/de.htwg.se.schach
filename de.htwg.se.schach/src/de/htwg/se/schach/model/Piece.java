bdc05d72d3f8d48e96ec3fc03c68aadfd31c8777
robertkleinhans <rokleinh@htwg-konstanz.de>
2016-05-27 10:20:24 +0200

    Adding new abstract piece.

--- de.htwg.se.schach/src/de/htwg/se/schach/model/Piece.java
+++ de.htwg.se.schach/src/de/htwg/se/schach/model/Piece.java
@@ -1,5 +1,31 @@
 package de.htwg.se.schach.model;
 
-public class Piece {
-
+public abstract class Piece {
+    final int MAX_ROW = 7;
+    final int MAX_COLUMN = 7;
+    public int row;
+    public int column;
+    public String cut;
+    public final int team;
+    
+    public Piece(int team) {
+        this.team = team;
+    }
+    
+    public int getRow() {
+        return row;
+    }
+    
+    public int getColumn() {
+        return column;
+    }
+    
+    public String getName() {
+        return cut;
+    }
+    
+    public int[] getPosition() {
+        int[] tmp = {row,column};
+        return tmp;
+    }
 }
