ab8ac3309c0b31e0f898fbc44d69ddb1897db9bd
robertkleinhans <rokleinh@htwg-konstanz.de>
2016-05-27 10:26:44 +0200

    Adding King.

---
+++ de.htwg.se.schach/src/de/htwg/se/schach/model/King.java
@@ -0,0 +1,42 @@
+package de.htwg.se.schach.model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class King extends Piece {
+    public King(int row, int column, int team) {
+        super(team);
+        this.row = row;
+        this.column = column;
+        this.cut = "KI";
+    }
+    
+    public List<Position> getAllMoves() {
+        List<Position> ret = new LinkedList<Position>();
+        
+        if(row+1 < 8) {
+            ret.add(new Position(row+1,column));
+            if(column+1 < 8) {
+                ret.add(new Position(row+1,column+1));
+                ret.add(new Position(row,column+1));
+            }
+            if(column-1 >= 0) {
+                ret.add(new Position(row+1,column-1));
+                ret.add(new Position(row,column-1));
+            }
+        }
+        if(row-1 >= 0) {
+            ret.add(new Position(row-1,column));
+            if(column+1 < 8) {
+                ret.add(new Position(row-1,column+1));
+            }
+            if(column-1 >= 0) {
+                ret.add(new Position(row-1,column-1));
+            }
+        }
+        
+        return ret;
+    }
+}
+
