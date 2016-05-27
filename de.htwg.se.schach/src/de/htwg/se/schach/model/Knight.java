6f47d81bf4adfff513a5dd6cf020c8345357fbed
robertkleinhans <rokleinh@htwg-konstanz.de>
2016-05-27 10:31:50 +0200

    Adding Knight.

---
+++ de.htwg.se.schach/src/de/htwg/se/schach/model/Knight.java
@@ -0,0 +1,54 @@
+package de.htwg.se.schach.model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Knight extends Piece {
+    public Knight(int row, int column, int team) {
+        super(team);
+        this.row = row;
+        this.column = column;
+        this.cut = "KN";
+    }
+    
+    public List<Position> getAllMoves() {
+        List<Position> ret = new LinkedList<Position>();
+        
+        if(row+2 < 8) {
+            if (column-1 >= 0) {
+                ret.add(new Position(row+2,column-1));
+            }
+            if (column+1 < 8) {
+                ret.add(new Position(row+2,column+1));
+            }
+        }
+        if(row+1 < 8) {
+            if (column-2 >= 0) {
+                ret.add(new Position(row+1,column-2));
+            }
+            if (column+2 < 8) {
+                ret.add(new Position(row+1, column+2));
+            }
+        }
+        if(row-2 >= 0) {
+            if(column-1 >= 0) {
+                ret.add(new Position(row-2,column-1));
+            }
+            if(column+1 < 8) {
+                ret.add(new Position(row-2,column+1));
+            }
+        }
+        if(row-1 >= 0) {
+            if(column-2 >= 0) {
+                ret.add(new Position(row-1,column-2));
+            }
+            if(column+2 < 8) {
+                ret.add(new Position(row-1,column+2));
+            }
+        }
+        
+        return ret;
+    }
+}
+
