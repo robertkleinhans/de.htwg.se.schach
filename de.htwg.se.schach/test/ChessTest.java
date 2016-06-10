c356a56759f0c7093f69428c3a3fc078c0682519
robertkleinhans <rokleinh@htwg-konstanz.de>
2016-06-10 12:59:19 +0200

    Completed ChessViewTest and added a very small test for Chess.

---
+++ de.htwg.se.schach/test/ChessTest.java
@@ -0,0 +1,13 @@
+
+
+import org.junit.Test;
+
+import de.htwg.se.schach.Chess;
+
+public class ChessTest {
+	
+	@Test
+	public void testChess() {
+		Chess.main(null);
+	}
+}
--- de.htwg.se.schach/test/de/htwg/se/schach/view/ChessViewTest.java
+++ de.htwg.se.schach/test/de/htwg/se/schach/view/ChessViewTest.java
@@ -5,7 +5,34 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import de.htwg.se.schach.control.Position;
+import de.htwg.se.schach.view.ChessView;
 
 public class ChessViewTest {
 
+	@Test
+	public void testChessView() {
+		ChessView view = new ChessView();
+		view.printField();
+	}
+	
+	@Test
+	public void testMovePiece() {
+		ChessView view = new ChessView();
+		assertTrue(view.movePiece(new Position(1,0), new Position(3,0), 0));
+	}
+	
+	@Test
+	public void testCheckPiece() {
+		ChessView view = new ChessView();
+		assertTrue(view.checkPiece(new Position(0,1), 0));
+	}
+	
+	@Test
+	public void testViewMovement() {
+		ChessView view = new ChessView();
+		view.viewMovement(new Position(0,1));
+		assertTrue(view.movePiece(new Position(0,1), new Position(2,2), 0));
+		assertTrue(view.movePiece(new Position(2,2), new Position(4,3), 0));
+		view.viewMovement(new Position(4,3));
+	}
 }
