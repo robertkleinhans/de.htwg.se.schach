a1171e61f8a8602effca2e09aa8f89ba7724440f
robertkleinhans <rokleinh@htwg-konstanz.de>
2016-06-30 20:33:04 +0200

    Fixed critical issue.

--- de.htwg.se.schach/src/de/htwg/se/schach/view/TuiInput.java
+++ de.htwg.se.schach/src/de/htwg/se/schach/view/TuiInput.java
@@ -3,6 +3,8 @@ package de.htwg.se.schach.view;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
+import org.apache.log4j.Logger;
+
 public class TuiInput extends Thread {
 	Signal sig;
 	
@@ -10,10 +12,14 @@ public class TuiInput extends Thread {
 		this.sig = sig;
 	}
 	
+	private static final Logger LOGGER = Logger.getLogger("de.htwg.se.schach.tuiInput");
+	
 	@Override
 	public void run() {
 		Scanner scan = new Scanner(System.in);
 		String holder;
+		
+		
 		while(!Thread.currentThread().isInterrupted()) {
 			try{
 				holder = scan.nextLine();
@@ -22,7 +28,7 @@ public class TuiInput extends Thread {
 					break;
 				}
 			} catch(NoSuchElementException e) {
-				//do nothing
+				LOGGER.info(e);
 			}
 		}
 		scan.close();
